108a2f59f9ca13e412c0fdd3512ec00bb7722645
Kader Genç <kadergen1@gmail.com>
2024-04-27 23:49:23 +0200

    Add server-client and establish connection using socket io

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+    console.log('Connected to server');
+
+    socket.on('message', (data) => {
+        console.log(`Message: ${data}`);
+
+        io.emit('message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnect');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server port: ${PORT}`);
+});
---
+++ socket-client.js
@@ -0,0 +1,12 @@
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
+
+socket.on('message', (data) => {
+    console.log(`Message from server: ${data}`);
+});
+
+function sendMessage(message) {
+    socket.emit('message', message);
+}
+
+sendMessage('Test message');
